07c0241088d0f851fa4bc61aefebd35d2beb7d40
Adam Bull <adamjamesbull@googlemail.com>
2021-01-13 18:29:07 +0000

    Return a message noting there are no list items.

--- frontend/src/components/List.js
+++ frontend/src/components/List.js
@@ -3,6 +3,13 @@ import React from "react";
 import "./List.css";
 
 const List = ({ items }) => {
+  if (!items.length) {
+    return (
+      <div>
+        No short URLs created yet
+      </div>
+    )
+  }
   return (
     <ul className="url-list">
       {items.map(({ code, url }) => (

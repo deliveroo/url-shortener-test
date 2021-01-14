33b7841cb1d34257a4d55df6504dc47bd00c59d8
Adam Bull <adamjamesbull@googlemail.com>
2021-01-14 15:39:30 +0000

    Use different redis db for test and dev

--- backend/config/initializers/redis.rb
+++ backend/config/initializers/redis.rb
@@ -1,1 +1,3 @@
-REDIS = Redis.new(host: ENV['IS_DOCKER'] ? 'redis' : 'localhost')
+db_number = Rails.env.development? ? 0 : 1
+puts "Selecting redis db at index #{db_number} based on Rails.env (#{Rails.env})"
+REDIS = Redis.new(host: ENV['IS_DOCKER'] ? 'redis' : 'localhost', db: db_number)
